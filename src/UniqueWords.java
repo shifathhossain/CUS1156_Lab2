369cb1fe7cb57d3ae61218be0cf82340ee491983
shftee <shifath.hossain2@gmail.com>
2024-09-30 17:44:48 -0400

    changes

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -10,28 +10,38 @@ public class UniqueWords
    public static int countUnique(ArrayList<String> list)
    {
 	  int count = 0;
-	  
+
       for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
+      {	
+         boolean isUnique = true;
+
+         for (int j = 0; j < i; j++)
+         {
+            if (list.get(i).equals(list.get(j))) {
+               isUnique = false;
+               break; 
+            }
+         }
+
+         if (isUnique) {
+            count++;
+         }
       }
 	  return count;
    }
 
    public static void main(String[] args)
    {
-      ArrayList <String> words = new ArrayList<>();
+      ArrayList<String> words = new ArrayList<>();
       words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
+      words.add("orange");
+      words.add("blackboard");
+      words.add("apple");
+      words.add("orange");
+      words.add("sun");
+      words.add("moon");
    
-	  int unique = countUnique(words);
+      int unique = countUnique(words);
       System.out.println(words + " has " + unique + " unique words");
    }
 }
